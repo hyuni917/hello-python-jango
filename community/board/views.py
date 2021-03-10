9b15585e2ab305035f27d141a42906a277ef5b8a
kyunghyunkim <kyunghyun@terafunding.com>
2021-03-10 13:32:41 +0900

    태그 등록 및 상세

--- community/board/views.py
+++ community/board/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.http import Http404
 from user.models import User
+from tag.models import Tag
 from .models import Board
 from .forms import BoardForm
 
@@ -23,11 +24,22 @@ def board_write(request):
         if form.is_valid():
             user_id = request.session.get('user')
             user = User.objects.get(pk=user_id)
+
+            tags = form.cleaned_data['tags'].split(',')
+
             board = Board()
             board.title = form.cleaned_data['title']
             board.contents = form.cleaned_data['contents']
             board.writer = user
             board.save()
+
+            for tag in tags:
+                if not tag:
+                    continue
+                # 모델에 있으면 가져오고 없으면 생성 후 가져온다.
+                # 파이썬에서 사용하지 않는 변수는 _ 세팅한다.
+                _tag, _ = Tag.objects.get_or_create(name=tag)
+                board.tags.add(_tag)
             return redirect('/board/list')
     else:
         form = BoardForm()
