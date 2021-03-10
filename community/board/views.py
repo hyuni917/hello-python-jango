2bd2bc09cb6666df69a72828cc7bdb06ed82e11a
kyunghyunkim <kyunghyun@terafunding.com>
2021-03-10 11:55:28 +0900

    board 리스트 페이징처리

--- community/board/views.py
+++ community/board/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from django.http import Http404
 from user.models import User
 from .models import Board
@@ -34,5 +35,8 @@ def board_write(request):
 
 
 def boar_list(request):
-    boards = Board.objects.all().order_by('-id')
+    all_boards = Board.objects.all().order_by('-id')
+    page = request.GET.get('p', 1)
+    paginator = Paginator(all_boards, 2)
+    boards = paginator.get_page(page)
     return render(request, 'board_list.html', {'boards': boards})
