343d16a12a3e1c165688e7ef3fdb5f86969357a4
kyunghyunkim <kyunghyun@terafunding.com>
2021-03-10 11:28:42 +0900

    로그인 예외처리

--- community/user/forms.py
+++ community/user/forms.py
@@ -6,12 +6,12 @@ from django.contrib.auth.hashers import check_password
 class LoginForm(forms.Form):
     username = forms.CharField(
         error_messages={
-            'required' : '아이디를 입력해주세요.'
+            'required': '아이디를 입력해주세요.'
         },
         max_length=32, label='사용자 이름')
     password = forms.CharField(
         error_messages={
-            'required' : '비밀번호를 입력해주세요.'
+            'required': '비밀번호를 입력해주세요.'
         },
         widget=forms.PasswordInput, label='비밀번호')
 
@@ -21,7 +21,11 @@ class LoginForm(forms.Form):
         password = cleaned_data.get('password')
 
         if username and password:
-            user = User.objects.get(username=username)
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                self.add_error('username', '아이디가 없습니다.')
+                return
             if not check_password(password, user.password):
                 self.add_error('password', '비밀번호를 틀렸습니다.')
             else:
